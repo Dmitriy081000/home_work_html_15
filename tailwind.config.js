c469759b3c8df1cb58717ce5912ae5f807ada7a1
Дмитро Височин <dmitriy081000@gmail.com>
2024-10-24 21:30:56 +0300

    home work 15

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html'],
+  theme: {
+    extend: {
+      textColor: {
+        black: 'black'
+      },
+      width: {
+        '80': '80%',
+      }
+    },
+  },
+  plugins: [],
+}
+
